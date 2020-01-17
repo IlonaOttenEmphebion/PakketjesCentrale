fa3535fff20beada9e6a5a0cb9b842a74f0c31d1
Ilona Otten <IOtten@conclusion.nl>
2020-01-17 12:11:34 +0100

    Start ParcelDataBaseApi en bijbehorende classes. Gradel en manifest aangepast.

---
+++ app/src/main/java/com/example/pakketjescentrale/data/ParcelDataBaseApiService.kt
@@ -0,0 +1,20 @@
+package com.example.pakketjescentrale.data
+
+import com.example.pakketjescentrale.model.Parcel
+import com.example.pakketjescentrale.model.ParcelResult
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ParcelDataBaseApiService {
+    @GET("/api​/v1​/parcels​/owned")
+    fun getMyParcels(): Call<ParcelResult>
+
+    @GET("/api​/v1​/parcels​/received")
+    fun getParcelsReceived(): Call<ParcelResult>
+
+    @POST("/api​/v1​/parcels​/received")
+    fun registerReceivedParcel(parcel: Parcel)
+
+    @PUT("/api/v1/parcels/{parcel_id}/pickup")
+    fun updateParcel(@Path("parcel_id") parcel_id: Number, parcel: Parcel)
+}
