0062778e9df747702284ef3944156fd97812a1be
Marlen Blazevic <marlen.blazevic@gmail.com>
2020-08-22 23:23:45 +0200

    HENK

--- app/src/main/java/com/example/pakketjescentrale/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/pakketjescentrale/ui/dashboard/DashboardFragment.kt
@@ -8,11 +8,17 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pakketjescentrale.R
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+    private val packages = arrayListOf<ParcelText>()
+    private val dashboardAdapter = DashboardAdapter(packages)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,5 +33,15 @@ class DashboardFragment : Fragment() {
             textView.text = it
         })
         return root
+
+        packages.add(ParcelText("Do Homework"))
+        packages.add(ParcelText("Answer Questions"))
     }
+
+//    private fun initViews() {
+//        // Initialize the recycler view with a linear layout manager, adapter
+//        rv_myParcels.layoutManager = LinearLayoutManager(this@DashboardFragment, RecyclerView.VERTICAL, false)
+//        rv_myParcels.adapter = dashboardAdapter
+//        rv_myParcels.addItemDecoration(DividerItemDecoration(this@DashboardFragment, DividerItemDecoration.VERTICAL))
+//    }
 }
