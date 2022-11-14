646280e7739e2fb696293f18c43a9d92d9930017
Marlen <marlen.blazevic@gmail.com>
2022-11-14 22:51:24 +0100

    Swipe away in recyclerview

--- app/src/main/java/com/example/pakketjescentrale/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/pakketjescentrale/ui/dashboard/DashboardFragment.kt
@@ -8,12 +8,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pakketjescentrale.R
 import com.example.pakketjescentrale.data.parceldatabase.ParcelRepository
 import com.example.pakketjescentrale.model.Parcel
 import com.example.pakketjescentrale.model.ParcelLocation
+import com.google.android.material.snackbar.Snackbar
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -37,6 +39,60 @@ class DashboardFragment : Fragment() {
 
         recyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         recyclerView.adapter = dashboardAdapter
+
+
+
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                // this method is called
+                // when the item is moved.
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // this method is called when we swipe our item to right direction.
+                // on below line we are getting the item at a particular position.
+                val deletedCourse: Parcel =
+                    packages.get(viewHolder.adapterPosition)
+
+                // below line is to get the position
+                // of the item at that position.
+                val position = viewHolder.adapterPosition
+
+                // this method is called when item is swiped.
+                // below line is to remove item from our array list.
+                packages.removeAt(viewHolder.adapterPosition)
+
+                // below line is to notify our item is removed from adapter.
+                dashboardAdapter.notifyItemRemoved(viewHolder.adapterPosition)
+
+                // below line is to display our snackbar with action.
+                Snackbar.make(
+                    recyclerView,
+                    "Deleted " + deletedCourse.parcelID.toString(),
+                    Snackbar.LENGTH_LONG
+                )
+                    .setAction(
+                        "Undo",
+                        View.OnClickListener {
+                            // adding on click listener to our action of snack bar.
+                            // below line is to add our item to array list with a position.
+                            packages.add(position, deletedCourse)
+
+                            // below line is to notify item is
+                            // added to our adapter class.
+                            dashboardAdapter.notifyItemInserted(position)
+                        }).show()
+            }
+            // at last we are adding this
+            // to our recycler view.
+        }).attachToRecyclerView(recyclerView)
+
+
 
         parcelRepository.getParcelsReceived().enqueue(object :
             Callback<Array<Parcel>> {
--- app/src/main/java/com/example/pakketjescentrale/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/pakketjescentrale/ui/notifications/NotificationsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pakketjescentrale.MainActivity
@@ -15,6 +16,7 @@ import com.example.pakketjescentrale.data.parceldatabase.ParcelRepository
 import com.example.pakketjescentrale.model.Parcel
 import com.example.pakketjescentrale.ui.newparcel.NewParcelActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -45,6 +47,56 @@ class NotificationsFragment : Fragment() {
 
         recyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         recyclerView.adapter = notificationsAdapter
+
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                // this method is called
+                // when the item is moved.
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // this method is called when we swipe our item to right direction.
+                // on below line we are getting the item at a particular position.
+                val deletedCourse: Parcel =
+                    packages.get(viewHolder.adapterPosition)
+
+                // below line is to get the position
+                // of the item at that position.
+                val position = viewHolder.adapterPosition
+
+                // this method is called when item is swiped.
+                // below line is to remove item from our array list.
+                packages.removeAt(viewHolder.adapterPosition)
+
+                // below line is to notify our item is removed from adapter.
+                notificationsAdapter.notifyItemRemoved(viewHolder.adapterPosition)
+
+                // below line is to display our snackbar with action.
+                Snackbar.make(
+                    recyclerView,
+                    "Deleted " + deletedCourse.parcelID.toString(),
+                    Snackbar.LENGTH_LONG
+                )
+                    .setAction(
+                        "Undo",
+                        View.OnClickListener {
+                            // adding on click listener to our action of snack bar.
+                            // below line is to add our item to array list with a position.
+                            packages.add(position, deletedCourse)
+
+                            // below line is to notify item is
+                            // added to our adapter class.
+                            notificationsAdapter.notifyItemInserted(position)
+                        }).show()
+            }
+            // at last we are adding this
+            // to our recycler view.
+        }).attachToRecyclerView(recyclerView)
 
         parcelRepository.getMyParcels().enqueue(object :
             Callback<Array<Parcel>> {
